c5e9986eece462489b7bf57f9f758c1d0c85187d
chen <zhaochensy@gmail.com>
2018-11-20 17:20:57 +0800

    登录访问控制完成

--- app.py
+++ app.py
@@ -5,30 +5,57 @@ from flask import Flask
 import os
 import time
 from flask import render_template
+from flask import redirect,url_for
+from flask import session, abort
+from functools import wraps
+from flask import make_response
+import uuid
+from datetime import timedelta
 
-import sys 
-reload(sys)  # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入 
+def login_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        uid = request.cookies.get('uid')
+        if not uid in session:
+            return redirect('/')
+        return func(*args, **kwargs)
+    return decorated_function
+
+# python2 需要使用,否则页面会乱码报错
+import sys
+reload(sys)  # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
 sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
+app.secret_key = '12345689'
 
 @app.route('/')
 def index():
-    #res = os.popen('ifconfig').read()
-    #return render_template("index.html")
-    return render_template("login1.html")
-
-#@app.route('/login', methods=['POST'])
-@app.route('/VerifyLogin', methods=['POST'])
-def login():
-    username = request.form['loginid']
-    password = request.form['userpassword']
-    if username == 'admin' and password=='admin':
-        res = "登录成功"
+    token = str(uuid.uuid1())
+    if os.path.exists('password.txt'):
+        resp = make_response(render_template('login.html', token=token))
     else:
-        res = "密码错误"
-    return render_template('welcome.html', name='巴音乔哥')
+        resp = make_response(render_template('index.html', token=token))
+    cookie = request.cookies.get('uid')
+    if cookie is None:
+        resp.set_cookie('uid',token)
+    return resp
 
+@app.route('/VerifyLogin', methods=['POST'])
+def verifyLogin():
+    password = request.form['password']
+    with open('password.txt') as f:
+        oldpasword = f.read()
+    if password == oldpasword:
+        uid = request.cookies.get('uid')
+        session[uid] = True
+        res = "success"
+    else:
+        res = "failed"
+    return res
+
+
+@login_required
 @app.route('/status',methods=['POST'])
 def status():
     res = os.popen('ifconfig eth0 | grep "inet"').read()
@@ -40,11 +67,13 @@ def status():
     return s
 
 
+@login_required
 @app.route('/changepassword')
 def changepassword():
     return render_template('changepassword.html',name="乔哥牛逼！！！！！！！！！！！！！")
 
 
+@login_required
 @app.route('/applylocal', methods=['POST'])
 def setIP():
     ip = request.form['ip']
@@ -71,6 +100,7 @@ def setIP():
     return "设置成功"
 
 
+@login_required
 @app.route('/applydhcp', methods=['POST'])
 def dhcp():
     lines = ['TYPE=Ethernet\n',
@@ -86,10 +116,32 @@ def dhcp():
     os.popen('systemctl restart network')
     return "DHCP 服务设置成功"
 
+@app.route('/createpassword', methods=['GET','POST'])
+def createPassword():
+    password1 = request.form['password1']
+    password2 = request.form['password2']
+    if password1 == password2 and not os.path.exists('password.txt'):
+        with open('password.txt','w') as f:
+            f.write(password1)
+        #uuid = request.cookies.get('user')
+        #u = user.User(uuid, True)
+        session['login'] = True
+        return 'success'
+    else:
+        return "Failed"
+
+
+@app.route('/main')
+@login_required
+def setmain():
+    return render_template('welcome.html')
 
 if __name__ == "__main__":
+    app.permanent_session_lifetime = timedelta(minutes=1)
     app.run(
         host='0.0.0.0',
         port=80,
         debug=True
     )
+
+
